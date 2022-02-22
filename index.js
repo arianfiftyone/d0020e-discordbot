dab12a79827b9a15c5d0b6b2c065960fda4bfcca
arianfiftyone <arian.masoudi@hotmail.com>
2022-02-22 13:58:11 +0100

    test

--- index.js
+++ index.js
@@ -1,17 +1,85 @@
-require('dotenv').config(); //initialize dotenv
-const Discord = require('discord.js'); //import discord.js
+require('dotenv').config();
+const { Client, Intents, WebhookClient } = require('discord.js'); 
+const cli = require('nodemon/lib/cli');
 
-const client = new Discord.Client({
-    intents: [
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES
+//create new client
+const client = new Client({
+    intents: [ 
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES 
     ]
-}); //create new client
+});
+
+const webhookClient = new WebhookClient(
+    process.env.WEBHOOK_ID,
+    process.env.WEBHOOK_TOKEN,
+)
+
+const PREFIX = '$';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
+
+client.on('messageCreate', (message) => {
+    
+    console.log(`[${message.author.tag}]: ${message.content}`);
+
+    //stops the infinite response from bot, author is user-type
+    if (message.author.bot) return;
+
+    if (message.content.startsWith(PREFIX)) {
+        
+        /* parsing commands: 
+            array destructuring on commandName
+            args is an array
+            ... is a spreader operator */
+        const [commandName, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/); //regex for whitespace
+
+
+        if (commandName === 'kick') {
+
+            if (args.length === 0) return message.reply('An ID needs to be provided');
+
+            //const member = message.guild.members.kick()
+            const member = message.guild.members.cache.get(args[0]);
+            
+            if (member ) {
+                member
+                    .kick() //returns a promise
+                    .then((member) => message.channel.send(`GTFO ${member}!`)) //handles promise
+                    .catch((err) => message.channel.send('I dont have those permissions to kick that user')) //if they dont have permissions
+
+        
+            } else {
+                message.channel.send('User was not found!')
+            }
+        } else if (commandName === 'announce') {
+            const msg = args.join(' ');
+            webhookClient.send(msg);
+        }
+
+    }
+
+
+
+    // random stuff tested
+    /* if (message.content === 'hello') {
+        tags the user
+        message.reply('hello there!');
+
+        replies to channel infinitely
+        message.channel.send('hello')
+
+    }; */
+
+});
+
+
 //make sure this line is the last line
-client.login(process.env.TOKEN); //login bot using token
+client.login(process.env.BOT_TOKEN); //login bot using token
